ba80bd180e1a68a4f60a55f4792ba7ba1071a15d
zavocc <buffer.0verfl0w1030@gmail.com>
2026-04-07 14:20:30 +0800

    add init logs

--- src/index.ts
+++ src/index.ts
@@ -45,6 +45,7 @@ for (const folder of commandFolders) {
     // Set a new item in the Collection with the key as the command name and the value as the exported module
     if ("data" in command && "execute" in command) {
       botClient.commands.set(command.data.name, command);
+      console.log(`[INFO] Loaded command: ${command.data.name} from ${filePath}`);
     } else {
       console.log(
         `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
@@ -64,6 +65,7 @@ for (const file of eventFiles) {
   } else {
     botClient.on(event.name, (...args) => event.execute(...args));
   }
+  console.log(`[INFO] Loaded event: ${event.name} from ${filePath}`);
 }
 
 // Start services and log in
