801ce0cbe339df08d22426222657e71ceec1b563
zavocc <buffer.0verfl0w1030@gmail.com>
2026-04-08 12:04:08 +0800

    update

--- src/lib/llm/tools/apis/WebSearch/schema.ts
+++ src/lib/llm/tools/apis/WebSearch/schema.ts
@@ -3,7 +3,7 @@ export const TOOL_SCHEMAS = [
     type: "function",
     function: {
       name: "web_search",
-      description: "Search the web for latest information.",
+      description: "v_2.0AlphaTODO[Search the web for latest information. To target or precisely browse specific URL, use site: operator",
       parameters: {
         type: "object",
         properties: {
